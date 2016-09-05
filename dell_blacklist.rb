e3ccfb8db11bd043efc4a8701ec7a1d278fdfac3
Sunit Parekh <parekh.sunit@gmail.com>
2016-09-05 14:04:03 +0530

    updated to latest 0.8.0 version the project and fixed issue with SelectFromDatabase strategy

--- dell_blacklist.rb
+++ dell_blacklist.rb
@@ -3,6 +3,8 @@ require 'data-anonymization'
 
 DataAnon::Utils::Logging.logger.level = Logger::INFO
 
+connection_spec = {:adapter => 'postgresql', :host => 'localhost', :port => 5432, :pool => 5, :username => 'developer', :password => 'password', :database => 'dell'}
+
 database 'DellStore' do
   strategy DataAnon::Strategy::Blacklist
   source_db({:adapter => 'postgresql', :host => 'localhost', :port => 5432, :pool => 5, :username => 'developer', :password => 'password', :database => 'dell'})
@@ -14,11 +16,11 @@ database 'DellStore' do
     anonymize('lastname').using FieldStrategy::RandomLastName.new
     anonymize('address1').using FieldStrategy::RandomAddress.region_US
     anonymize('address2').using FieldStrategy::RandomString.new
-    anonymize('state').using FieldStrategy::SelectFromDatabase.new("customers","state")
+    anonymize('state').using FieldStrategy::SelectFromDatabase.new("customers","state", connection_spec)
     anonymize('zip').using FieldStrategy::RandomZipcode.region_US
     anonymize('email').using FieldStrategy::RandomMailinatorEmail.new
     anonymize('phone').using FieldStrategy::RandomPhoneNumber.new
-    anonymize('creditcardtype').using FieldStrategy::SelectFromDatabase.new("customers","creditcardtype")
+    anonymize('creditcardtype').using FieldStrategy::SelectFromDatabase.new("customers","creditcardtype", connection_spec)
     anonymize('creditcard').using FieldStrategy::RandomPhoneNumber.new
     anonymize('username').using FieldStrategy::StringTemplate.new('user#{row_number}')
     anonymize('password') { |f| 'password'}
--- dell_whitelist.rb
+++ dell_whitelist.rb
@@ -1,7 +1,8 @@
-
 require 'data-anonymization'
 
 DataAnon::Utils::Logging.logger.level = Logger::INFO
+
+connection_spec = {:adapter => 'postgresql', :host => 'localhost', :port => 5432, :pool => 5, :username => 'developer', :password => 'password', :database => 'dell'}
 
 database 'DellStore' do
   strategy DataAnon::Strategy::Whitelist
@@ -36,11 +37,11 @@ database 'DellStore' do
     anonymize('lastname').using FieldStrategy::RandomLastName.new
     anonymize('address1').using FieldStrategy::RandomAddress.region_US
     anonymize('address2').using FieldStrategy::RandomString.new
-    anonymize('state').using FieldStrategy::SelectFromDatabase.new("customers","state")
+    anonymize('state').using FieldStrategy::SelectFromDatabase.new("customers","state", connection_spec)
     anonymize('zip').using FieldStrategy::RandomZipcode.region_US
     anonymize('email').using FieldStrategy::RandomMailinatorEmail.new
     anonymize('phone').using FieldStrategy::RandomPhoneNumber.new
-    anonymize('creditcardtype').using FieldStrategy::SelectFromDatabase.new("customers","creditcardtype")
+    anonymize('creditcardtype').using FieldStrategy::SelectFromDatabase.new("customers","creditcardtype", connection_spec)
     anonymize('creditcard').using FieldStrategy::RandomPhoneNumber.new
     anonymize('username').using FieldStrategy::StringTemplate.new('user#{row_number}')
     anonymize('password') { |f| 'password'}
